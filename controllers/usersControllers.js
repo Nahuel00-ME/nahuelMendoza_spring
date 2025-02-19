0bc2ad4d96693c8e5182fe33e9c13b53c3fc87be
Nahuel Ezequiel Mendoza <ezequielnahuel11@gmail.com>
2025-02-18 23:12:00 -0300

    subo spring 4 y spring 5 incompletos

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -7,15 +7,6 @@ const usersControllers = {
     register: function(req, res, next) {
         res.render("users/register",{title : 'registro de usuario'})
 }
-,
-index:(req, res) =>{
-    res.render('index',{
-        index: nose.index,
-        carrito: nose.carrito,
-        login :nose.login,
-        register: nose.register,
-    })
-}
 };
 
  module.exports = usersControllers;
