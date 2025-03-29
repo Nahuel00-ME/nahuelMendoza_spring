6bcf7eb75f019ffa37fb75a7a26830692049f17c
Eric <menaericdaniel@gmail.com>
2025-03-29 17:26:28 -0300

    auth user done!

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var router = express.Router();
-const { login, register,procesoRegistro, procesoLogin,profile,logout} = require("../controllers/usersControllers");
-const check=require("../middelware/userSessionCheck");
-const checkAdmin=require("../middelware/adminCheck");
+const { login, register,procesoRegistro, procesoLogin,profile,logout, update} = require("../controllers/usersControllers");
+const check=require("../middlewares/userSessionCheck");
+const checkAdmin=require("../middlewares/adminCheck");
 router.get('/login' ,login);
 router.post('/loginProceso', procesoLogin);
 
 router.get('/register', register);
 router.post('/register', procesoRegistro);
 
-router.get('/profile/:id',check,checkAdmin,profile);
-router.post ('/profile/:id',)
+router.get('/profile',check,profile);
+router.post ('/profile',update)
 router.get('/logout',check,logout);
 
 
